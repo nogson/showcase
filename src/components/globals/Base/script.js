e2dc0d311014565f96bf21795d440c7e8c134913
佐藤文彦 <fhsato@willgroup.co.jp>
2018-01-19 19:00:48 +0900

    no message

--- src/components/globals/Base/script.js
+++ src/components/globals/Base/script.js
@@ -1,11 +1,9 @@
 
 import SideNavi from '../SideNavi/SideNavi.vue';
-import MainContent from '../MainContent/MainContent.vue';
 
 export default {
   name: "Base",
   components: {
-    'MainContent': MainContent,
     'SideNavi': SideNavi
   },
   data() {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import App from './components/globals/App/App.vue';
 import Base from './components/globals/Base/Base.vue';
+import Top from './components/pages/top/Top.vue';
+import Sample1 from './components/pages/sample1/Sample1.vue';
 
 Vue.use(Router);
 
@@ -11,8 +13,20 @@ new Vue({
   router: new Router({
     routes:
       [
-        { path: '/sub', component: { template: '<div>SUB</div>' } },
-        { path: '/', component: Base }
+        {
+          path: '/',
+          component: Base,
+          children: [
+            {
+              path: '/',
+              component: Top
+            },
+            {
+              path: '/sample1',
+              component: Sample1
+            }
+          ]
+        }
       ]
   })
 });
